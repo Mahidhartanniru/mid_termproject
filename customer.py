78ee42f4dc72dd254baa25208f890773b11d649b
Mahidhar Tanniru <87277264+Mahidhartanniru@users.noreply.github.com>
2024-02-27 21:51:06 -0500

    Update customer.py

--- customer.py
+++ customer.py
@@ -4,3 +4,13 @@ import json
 parser = argparse.ArgumentParser()
 parser.add_argument('filename', nargs='?', default='example_orders.json')
 args = parser.parse_args()
+
+customer = {}
+with open (args.filename,'r') as f:
+    data = json.load(f)
+    for order in data:
+        name =order["name"]
+        phone = order ["phone"]
+        customer[name]=phone
+with open('customers.json','w') as f:
+    json.dump(customer,f)
